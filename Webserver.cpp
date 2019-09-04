37679ad8c1dba2bd13511c7d71bcdd47fe15c5f5
MarkusNTrains <mraehle@gmail.com>
2019-09-04 15:52:50 +0200

    Update Webserver.cpp
    unsuded code removed

--- Webserver.cpp
+++ Webserver.cpp
@@ -88,16 +88,6 @@ WebServer::WebServer(void)
 
     Ethernet.begin(mac, ip);  // initialize Ethernet device
 
-    // Check for Ethernet hardware present
-    /*if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-        Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-        while (true) {
-            delay(1); // do nothing, no point running without Ethernet hardware
-        }
-    }
-    if (Ethernet.linkStatus() == LinkOFF) {
-        Serial.println("Ethernet cable is not connected.");
-    }*/
 
     // start the server
     m_server.begin();           // start to listen for clients
