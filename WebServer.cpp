d7875745baad2e981b76bf22e7507870c9e0d4d8
MarkusNTrains <mraehle@gmail.com>
2019-10-09 22:27:39 +0200

    Rechtschreibung korrigiert

--- WebServer.cpp
+++ WebServer.cpp
@@ -51,14 +51,14 @@
 //*****************************************************************************
 WebServer::WebServer(LedStrip* led_strip)
 {
-    int cnt;
+    int pin;
 		
     byte mac[] = { 0x10, 0x0D, 0x7F, 0xBF, 0xCA, 0x49 }; // MAC address from Ethernet shield sticker under board
     IPAddress ip(192, 168, 0, 250);    // IP address, may need to change depending on network
     m_server = new EthernetServer(80);            // server
     m_led_strip = led_strip;
     
-    // disable Ethernet chip
+    // disable Ethernet chip slave select
     pinMode(10, OUTPUT);
     digitalWrite(10, HIGH);
     
@@ -83,11 +83,11 @@ WebServer::WebServer(LedStrip* led_strip)
     
     
     // pins 26 to 49 are outputs
-/*    for (cnt = 26; cnt <= 49; cnt++) 
+    for (pin = 26; pin <= 49; pin++) 
 	  {
-        pinMode(i, OUTPUT);    // set pins as outputs
-        digitalWrite(i, LOW);  // switch the output pins off
-    }*/
+        pinMode(pin, OUTPUT);    // set pins as outputs
+        digitalWrite(pin, LOW);  // switch the output pins off
+    }
 
     Ethernet.begin(mac, ip);  // initialize Ethernet device
 
@@ -121,15 +121,16 @@ void WebServer::Tasks()
 	  {
         boolean currentLineIsBlank = true;
         while (client.connected()) 
-		{
+		    {
             if (client.available())    // client data available to read
-			{
+			      {
                 char c = client.read(); // read 1 byte (character) from client
+                        Serial.print(c);
                 // limit the size of the stored received HTTP request
                 // buffer first part of HTTP request in HTTP_req array (string)
                 // leave last element in array as 0 to null terminate string (REQ_BUF_SZ - 1)
                 if (req_index < (REQ_BUF_SZ - 1)) 
-			    {
+			          {
                     HTTP_req[req_index] = c;          // save HTTP request character
                     req_index++;
                 }
@@ -137,14 +138,16 @@ void WebServer::Tasks()
                 // last line of client request is blank and ends with \n
                 // respond to client only after last line received
                 if (c == '\n' && currentLineIsBlank) 
-			    {
+			          {
+                        Serial.println("last_line");
+
                     // send a standard http response header
                     client.println("HTTP/1.1 200 OK");
                     // remainder of header follows below, depending on if
                     // web page or XML page is requested
                     // Ajax request - send XML file
                     if (StrContains(HTTP_req, "ajax_inputs")) 
-			        {
+			              {
                         // send rest of HTTP header
                         client.println("Content-Type: text/xml");
                         client.println("Connection: keep-alive");
@@ -155,7 +158,7 @@ void WebServer::Tasks()
                         XML_response(client);
                     }
                     else   // web page request
-			        {
+			              {
                         // send rest of HTTP header
                         client.println("Content-Type: text/html");
                         client.println("Connection: keep-alive");
@@ -163,9 +166,9 @@ void WebServer::Tasks()
                         // send web page
                         webFile = SD.open("index.htm");        // open web page file
                         if (webFile) 
-			            {
+			                  {
                             while(webFile.available()) 
-			                {
+			                      {
                                 client.write(webFile.read()); // send web page to client
                             }
                             webFile.close();
@@ -182,13 +185,13 @@ void WebServer::Tasks()
                 
                 // every line of text received from the client ends with \r\n
                 if (c == '\n') 
-				{
+				        {
                     // last character on line of received text
                     // starting new line with next character read
                     currentLineIsBlank = true;
                 } 
                 else if (c != '\r') 
-				{
+				        {
                     // a text character was received from client
                     currentLineIsBlank = false;
                 }
