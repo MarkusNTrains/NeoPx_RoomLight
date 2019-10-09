3b5364c7e2f680973bac3ec1cff5ed53a4c51355
MarkusNTrains <mraehle@gmail.com>
2019-10-09 15:18:55 +0200

    Update WebServer.cpp
    brakets moved

--- WebServer.cpp
+++ WebServer.cpp
@@ -121,15 +121,15 @@ void WebServer::Tasks()
 	  {
         boolean currentLineIsBlank = true;
         while (client.connected()) 
-		    {
+		{
             if (client.available())    // client data available to read
-			      {
+			{
                 char c = client.read(); // read 1 byte (character) from client
                 // limit the size of the stored received HTTP request
                 // buffer first part of HTTP request in HTTP_req array (string)
                 // leave last element in array as 0 to null terminate string (REQ_BUF_SZ - 1)
                 if (req_index < (REQ_BUF_SZ - 1)) 
-				        {
+			    {
                     HTTP_req[req_index] = c;          // save HTTP request character
                     req_index++;
                 }
@@ -137,14 +137,14 @@ void WebServer::Tasks()
                 // last line of client request is blank and ends with \n
                 // respond to client only after last line received
                 if (c == '\n' && currentLineIsBlank) 
-				        {
+			    {
                     // send a standard http response header
                     client.println("HTTP/1.1 200 OK");
                     // remainder of header follows below, depending on if
                     // web page or XML page is requested
                     // Ajax request - send XML file
                     if (StrContains(HTTP_req, "ajax_inputs")) 
-					          {
+			        {
                         // send rest of HTTP header
                         client.println("Content-Type: text/xml");
                         client.println("Connection: keep-alive");
@@ -155,7 +155,7 @@ void WebServer::Tasks()
                         XML_response(client);
                     }
                     else   // web page request
-					          {
+			        {
                         // send rest of HTTP header
                         client.println("Content-Type: text/html");
                         client.println("Connection: keep-alive");
@@ -163,9 +163,9 @@ void WebServer::Tasks()
                         // send web page
                         webFile = SD.open("index.htm");        // open web page file
                         if (webFile) 
-						            {
+			            {
                             while(webFile.available()) 
-							              {
+			                {
                                 client.write(webFile.read()); // send web page to client
                             }
                             webFile.close();
@@ -182,13 +182,13 @@ void WebServer::Tasks()
                 
                 // every line of text received from the client ends with \r\n
                 if (c == '\n') 
-				        {
+				{
                     // last character on line of received text
                     // starting new line with next character read
                     currentLineIsBlank = true;
                 } 
                 else if (c != '\r') 
-				        {
+				{
                     // a text character was received from client
                     currentLineIsBlank = false;
                 }
